7ab1b7248c92c39585dfe6c712f9aee378f47060
Kato Ryota <kato.ryota.0124@gmail.com>
2021-05-18 14:34:33 +0900

    初期コミット。

--- employees-api/src/main/java/com/example/employeesapi/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.employeesapi.repository;
-
-
-import com.example.employeesapi.entity.Employee;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-}
--- employees-app/src/main/java/com/example/employeesapp/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.employeesapp.repository;
-
-import com.example.employeesapp.entity.Employee;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
-}
