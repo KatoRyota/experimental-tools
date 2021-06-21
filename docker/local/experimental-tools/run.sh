0ac84ed68f915063ceabf092627d75b3c98f623a
KatoRyota <kato.ryota.0124@gmail.com>
2021-06-21 17:50:02 +0900

    初回コミット。

--- docker/local/experimental-tools/run.sh
+++ docker/local/experimental-tools/run.sh
@@ -4,10 +4,9 @@ SHELL_SCRIPT_PATH=$0
 SHELL_SCRIPT_FILE_NAME=$(basename $SHELL_SCRIPT_PATH)
 SHELL_SCRIPT_DIR=$(cd $(dirname $SHELL_SCRIPT_PATH); pwd)
 
-cd /app/experimental-tools/ || exit 1
-
 echo "[Start] $SHELL_SCRIPT_DIR/$SHELL_SCRIPT_FILE_NAME"
 
+cd /app/experimental-tools/ || exit 1
 ./mvnw install || exit 1
 ./mvnw spring-boot:run -pl chat-api &
 ./mvnw spring-boot:run -pl employees-api &
