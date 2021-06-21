ef2d574e9fed0cec4af1d84c5ddc167820a9fe55
KatoRyota <kato.ryota.0124@gmail.com>
2021-06-21 12:41:23 +0900

    初回コミット。

---
+++ docker/local/experimental-tools/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /app/experimental-tools/ || exit 1
+: > stdout
+./mvnw install >> stdout 2>&1 < /dev/null || exit 1
+./mvnw spring-boot:run -pl chat-api >> stdout 2>&1 < /dev/null &
+./mvnw spring-boot:run -pl employees-api >> stdout 2>&1 < /dev/null &
--- docker/local/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /app/experimental-tools/ && \
-./mvnw install && \
-./mvnw spring-boot:run -pl chat-api && \
-./mvnw spring-boot:run -pl employees-api
